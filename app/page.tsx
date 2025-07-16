2e8cfff6dbe0d3ef00ecfe55135e407751a94c93
bimengzhou <dylanmengzhou@gmail.com>
2025-07-17 04:44:07 +0900

    更新主页标题为“Beaver Nail Inc.”，同时调整按钮样式为黑色文字白色背景

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-8 bg-[url('https://picsum.photos/id/29/4000/2670')] bg-cover bg-center">
-      <h1 className="text-5xl font-bold text-black bg-white p-3 rounded-xl">Beaver Nail</h1>
+      <h1 className="text-5xl font-bold text-black bg-white p-3 rounded-xl">
+        Beaver Nail Inc.
+      </h1>
       <button
-        className="bg-black text-white p-2 rounded-md font-bold"
+        className="bg-white text-black p-2 rounded-md font-bold"
         onClick={() => {
           console.log("clicked");
         }}
