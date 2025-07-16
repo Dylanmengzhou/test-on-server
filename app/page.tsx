8528c20fe3535a0dbe40eeff5791583e852583b8
bimengzhou <dylanmengzhou@gmail.com>
2025-07-17 03:59:25 +0900

    更新主页样式，调整标题字体大小和按钮颜色，同时修改背景样式

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Beaver Nail</h1>
+    <div className="flex flex-col items-center justify-center h-screen gap-8 bg-[url('https://picsum.photos/id/29/4000/2670')] bg-cover bg-center">
+      <h1 className="text-5xl font-bold text-black bg-white p-3 rounded-xl">Beaver Nail</h1>
       <button
-        className="bg-white text-black p-2 rounded-md"
+        className="bg-black text-white p-2 rounded-md font-bold"
         onClick={() => {
           console.log("clicked");
         }}
